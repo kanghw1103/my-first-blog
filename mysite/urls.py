eb2b594a4b202cce61da7721fc5c3e343f542d65
khw <kanghw1103@naver.com>
2019-10-15 21:15:13 +0900

    dsds

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views                           # !!!
 
 urlpatterns = [
+    path('', views.homepage, name='home'),    # !!!
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
+    # path('', include('blog.urls')),
 ]
